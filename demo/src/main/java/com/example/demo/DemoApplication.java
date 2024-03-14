fe965571d359f53ece39078ee3799ac099befb2d
Ikiriza <collingensi5@gmail.com>
2024-03-14 19:11:41 +0300

    new changes

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,26 @@
-package com.example.demo;
+package com.example.polls;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
+
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
 
 @SpringBootApplication
-public class DemoApplication {
+@EntityScan(basePackageClasses = { 
+		PollsApplication.class,
+		Jsr310JpaConverters.class 
+})
+public class PollsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+	@PostConstruct
+	void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
 	}
 
+	public static void main(String[] args) {
+		SpringApplication.run(PollsApplication.class, args);
+	}
 }
